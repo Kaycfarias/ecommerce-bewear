84afc5c5b8c1323f80a5232f69c51199a25ab339
Kaycfarias <kaycg123@gmail.com>
2025-09-08 17:27:25 -0300

    feat: add checkout success page

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -2,23 +2,13 @@
 
 import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogTitle,
-} from "@/components/ui/dialog";
+
 import { useFinishOrder } from "@/hooks/mutations/use-finish-order";
 import { loadStripe } from "@stripe/stripe-js";
 import { Loader2 } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
 
 const FinishOrderButton = () => {
   const finishOrderMutation = useFinishOrder();
-  const [successDialogIsOpen, setSuccessDialogIsOpen] = useState(false);
   const handleFinishOrder = async () => {
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
       throw new Error("Stripe publishable key is not set");
@@ -50,35 +40,6 @@ const FinishOrderButton = () => {
         )}
         Finalizar compra
       </Button>
-      <Dialog open={successDialogIsOpen} onOpenChange={setSuccessDialogIsOpen}>
-        <DialogContent className="text-center">
-          <Image
-            src="/illustration.svg"
-            alt="Success"
-            className="mx-auto"
-            width={300}
-            height={300}
-          />
-          <DialogTitle className="mt-4 text-2xl">Compra finalizada</DialogTitle>
-          <DialogDescription className="font-medium">
-            Seu pedido foi recebido com sucesso! você pode acompanhar o status
-            do seu pedido na seção de &quot;Meus Pedidos&quot;.
-          </DialogDescription>
-          <DialogFooter>
-            <Button className="rounded-full" size={"lg"} variant={"default"}>
-              Ver meus pedidos
-            </Button>
-            <Button
-              className="rounded-full"
-              size={"lg"}
-              variant={"outline"}
-              asChild
-            >
-              <Link href="/">Voltar para loja</Link>
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
     </>
   );
 };
---
+++ src/app/checkout/success/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import Image from "next/image";
+import Link from "next/link";
+
+const CheckoutSuccessPage = () => {
+  return (
+    <Dialog open={true} onOpenChange={() => {}}>
+      <DialogContent className="text-center">
+        <Image
+          src="/illustration.svg"
+          alt="Success"
+          className="mx-auto"
+          width={300}
+          height={300}
+        />
+        <DialogTitle className="mt-4 text-2xl">Compra finalizada</DialogTitle>
+        <DialogDescription className="font-medium">
+          Seu pedido foi recebido com sucesso! você pode acompanhar o status do
+          seu pedido na seção de &quot;Meus Pedidos&quot;.
+        </DialogDescription>
+        <DialogFooter className="sm:justify-center">
+          <Button className="rounded-full" size={"lg"} variant={"default"}>
+            Ver meus pedidos
+          </Button>
+          <Button
+            className="rounded-full"
+            size={"lg"}
+            variant={"outline"}
+            asChild
+          >
+            <Link href="/">Voltar para loja</Link>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CheckoutSuccessPage;
