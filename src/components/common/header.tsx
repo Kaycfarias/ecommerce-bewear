e2cd44d86c88d92e9cdd0e5bcb97e79d13189656
Kaycfarias <kaycg123@gmail.com>
2025-09-16 20:01:39 -0300

    feat(header): enhance header component with new navigation links and improved layout

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -5,14 +5,17 @@ import { useCart } from "@/hooks/queries/use-cart";
 import { authClient } from "@/lib/auth-client";
 import { AvatarFallback } from "@radix-ui/react-avatar";
 import {
+  HomeIcon,
   LogInIcon,
   LogOutIcon,
-  MenuIcon,
-  ShoppingBasketIcon,
+  Menu,
+  ShoppingBag,
+  Truck,
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Avatar, AvatarImage } from "../ui/avatar";
+import { Separator } from "../ui/separator";
 import {
   Sheet,
   SheetClose,
@@ -29,25 +32,21 @@ const Header = () => {
   const hasCartItems = cart && cart.items && cart.items.length > 0;
 
   return (
-    <header className="flex items-center justify-center p-5 mb-5">
+    <header className="flex items-center justify-center p-5 mb-3 border-b">
       <div className="flex items-center w-full justify-between">
         <Link href="/">
           <Image src="/next.svg" alt="Logo" width={120} height={26.14} />
         </Link>
-        <div className="flex items-center gap-3">
-          <Button asChild variant="outline" size="icon" className="relative">
-            <Link href="/cart">
-              <ShoppingBasketIcon />
-              {hasCartItems && (
-                <div className="absolute -top-1 -right-1 w-3 h-3 bg-red-500 rounded-full" />
-              )}
-            </Link>
-          </Button>
+        <div className="flex items-center space-x-3">
+          <Link href="/cart">
+            <ShoppingBag size={24} strokeWidth={1.2} />
+            {hasCartItems && (
+              <div className="absolute -top-1 -right-1 w-3 h-3 bg-red-500 rounded-full" />
+            )}
+          </Link>
           <Sheet>
             <SheetTrigger asChild>
-              <Button variant="outline" size="icon">
-                <MenuIcon />
-              </Button>
+              <Menu strokeWidth={1.2} size={24} />
             </SheetTrigger>
             <SheetContent>
               <SheetHeader>
@@ -55,7 +54,7 @@ const Header = () => {
               </SheetHeader>
               <div className="px-5">
                 {session?.user ? (
-                  <>
+                  <div className="space-y-3">
                     <div className="flex justify-between space-y-3">
                       <div className="flex items-center gap-3">
                         <Avatar>
@@ -77,15 +76,39 @@ const Header = () => {
                           </span>
                         </div>
                       </div>
-                      <Button
-                        variant={"outline"}
-                        size="icon"
-                        onClick={() => authClient.signOut()}
-                      >
-                        <LogOutIcon />
-                      </Button>
                     </div>
-                  </>
+                    <div className="p-3">
+                      <Separator />
+                    </div>
+                    <SheetClose asChild>
+                      <Link className="flex items-center" href="/">
+                        <HomeIcon size={16} className="mr-2" />
+                        <p className="text-sm">Início</p>
+                      </Link>
+                    </SheetClose>
+                    <SheetClose asChild>
+                      <Link className="flex items-center" href="/orders">
+                        <Truck size={16} className="mr-2" />
+                        <p className="text-sm">Meus pedidos</p>
+                      </Link>
+                    </SheetClose>
+                    <SheetClose asChild>
+                      <Link className="flex items-center" href="/cart">
+                        <ShoppingBag size={16} className="mr-2" />
+                        <p className="text-sm">Carrinho</p>
+                      </Link>
+                    </SheetClose>
+                    <div className="p-3">
+                      <Separator />
+                    </div>
+                    <div
+                      className="flex items-center cursor-pointer"
+                      onClick={() => authClient.signOut()}
+                    >
+                      <LogOutIcon color="#6a7282" size={16} className="mr-2" />
+                      <p className="text-sm text-gray-500">Sair da conta</p>
+                    </div>
+                  </div>
                 ) : (
                   <div className="flex items-center justify-between">
                     <h2 className="font-semibold">Olá, faça seu login</h2>
